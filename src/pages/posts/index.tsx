71bf5c755a1af879c9c71e333f1ff63d17dc5ee3
hugobianquini <hugobianqui@hotmail.com>
2022-04-21 14:00:53 -0300

    post exhibition page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,7 +3,8 @@ import Head from 'next/head';
 import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
-import {RichText} from 'prismic-dom';
+import { RichText } from 'prismic-dom';
+import Link from 'next/link';
 
 type Post = {
     slug: string,
@@ -26,11 +27,13 @@ export default function Posts({posts}: PostsProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     { posts.map(post => (
-                        <a key={post.slug} href="#">
-                        <time>{post.updatedAt}</time>
-                        <strong>{post.title}</strong>
-                        <p>{post.excerpt}</p>
-                    </a>
+                        <Link href={`/posts/${post.slug}`} key={post.slug}>
+                          <a>
+                           <time>{post.updatedAt}</time>
+                           <strong>{post.title}</strong>
+                           <p>{post.excerpt}</p>
+                          </a>
+                    </Link>
                     )) }
                     
                 </div>
@@ -51,7 +54,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
     const posts = response.results.map(post => {
         return {
-            slug: post,
+            slug: post.uid,
             title: RichText.asText(post.data.title),
             excerpt: post.data.content.find(content => content.type === "paragraph")?.text ?? '',
             updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {

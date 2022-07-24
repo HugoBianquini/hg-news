2bda92dbbba3717b2d5b10511fbfb947da75e2a5
hugobianquini <hugobianqui@hotmail.com>
2022-07-24 12:06:18 -0300

    setup coverage

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,12 @@ module.exports = {
 	testEnvironment: 'jsdom',
     moduleNameMapper: {
         "\\.(scss|css|sass)$": "identity-obj-proxy"
-    }
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.tsx",
+        "!src/**/*.test.tsx",
+        "!src/**/_app.tsx",
+        "!src/**/_document.tsx"
+    ]
 };
